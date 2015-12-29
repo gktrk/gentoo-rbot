fa3f10d8bd80dbdfb8d2a7ad93c3eab1be95e64c
Gokturk Yuksek <gokturk@binghamton.edu>
2015-12-29 00:29:30 -0500

    plugins: replace in repo rbot-gentoo plugin with the submodule
    Signed-off-by: Gokturk Yuksek <gokturk@binghamton.edu>

--- plugins/gentoo-scripts/earch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/usr/bin/python `dirname $0`/earch $1 | tr '\n' ' '
-echo
--- plugins/gentoo-scripts/herds.sh
+++
@@ -1,5 +0,0 @@
-HERDS_SRC='https://api.gentoo.org/packages/herds.xml'
-wget -O herds.xml -q "${HERDS_SRC}"
-if [ ! -s herds.xml ]; then
-	cp /usr/portage/metadata/herds.xml .
-fi
